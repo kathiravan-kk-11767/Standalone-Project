dad205cbd562d7558a84dbf5f9f9ad4c6b5d0455
kathi Zeal <kathiravan.kk@zohocorp.com>
2025-07-11 12:25:32 +0530

    First style changes

---
+++ AppSuite/Resources/AppSuiteTemplateSelector.cs
@@ -0,0 +1,32 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Zoho.UWP.Common.Theme.Model;
+using Zoho.UWP.Components.Theme.Model;
+
+namespace Zoho.UWP.AppSuite.Resources
+{
+    /// <summary><see cref="DataTemplateSelector"/> for app theme selector view</summary>
+    public class ThemeTemplateSelector : DataTemplateSelector
+    {
+        /// <summary>Default accent color template selector</summary>
+        public DataTemplate AccentColorSelectorTemplate { get; set; }
+        /// <summary>Template selector for custom color theme</summary>
+        public DataTemplate CustomColorThemeTemplate { get; set; }
+        /// <summary>Template selector for Windows color theme</summary>
+        public DataTemplate WindowsColorThemeTemplate { get; set; }
+
+        protected override DataTemplate SelectTemplateCore(object item, DependencyObject container)
+        {
+            ZAccentColorVObj themeBObj = item as ZAccentColorVObj;
+            //switch (themeBObj.Theme)
+            //{
+            //    case ThemeMode.CustomColor:
+            //return CustomColorThemeTemplate;
+            //    case ThemeMode.WindowsColor:
+            //        return WindowsColorThemeTemplate;
+            //    default:
+            return AccentColorSelectorTemplate;
+            //}
+        }
+    }
+}
---
+++ AppSuite/Resources/ZCommandBarResource.cs
@@ -0,0 +1,18 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Zoho.UWP.AppSuite.Resources
+{
+    public sealed class ZCommandBarResource : DependencyObject
+    {
+        public CommandBarDefaultLabelPosition DefaultLabelPosition
+        {
+            get { return (CommandBarDefaultLabelPosition)GetValue(DefaultLabelPositionProperty); }
+            set { SetValue(DefaultLabelPositionProperty, value); }
+        }
+
+        public static readonly DependencyProperty DefaultLabelPositionProperty =
+            DependencyProperty.Register("DefaultLabelPosition", typeof(CommandBarDefaultLabelPosition), typeof(ZCommandBarResource),
+                new PropertyMetadata(CommandBarDefaultLabelPosition.Collapsed));
+    }
+}
